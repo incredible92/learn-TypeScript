7790b04f3b7a794c1be6b9c20be4894a87609d3c
incredible92 <sulymanabdullahi@gmail.com>
2021-04-29 13:04:51 +0100

    feat: calculation

--- multiplier.ts
+++ multiplier.ts
@@ -2,5 +2,21 @@ const multiplicator = (a, b, printText) => {
     console.log(printText,  a * b);
   }
   
-  multiplicator(2, 4, 'Multiplied numbers 2 and 4, the result is:');
-  
+  multiplicator('how about a string?', 4, 'Multiplied a string and 4, the result is:');
+
+  type Operation = 'multiply' | 'add' | 'division';
+
+  type calculatorfn = number;
+
+  const calculator = (a:number, b: number, op: Operation) => {
+    if(op==='multiply'){
+      return a * b;
+    } else if(op==='add') {
+      return a + b;
+    } else if(op==="division"){
+      return a / b;
+    } 
+   
+  }
+
+  calculator(2, 4, 'null')
