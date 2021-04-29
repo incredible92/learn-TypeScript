adb4f26bbbc072b7b60aadcf22f7771eaf9f3553
incredible92 <sulymanabdullahi@gmail.com>
2021-04-29 13:26:51 +0100

    feat:using switch case

--- multiplier.ts
+++ multiplier.ts
@@ -4,19 +4,28 @@ const multiplicator = (a, b, printText) => {
   
   multiplicator('how about a string?', 4, 'Multiplied a string and 4, the result is:');
 
-  type Operation = 'multiply' | 'add' | 'division';
+  type Operation = 'multiply' | 'add' | 'divide';
 
   type calculatorfn = number;
+  type Result = string | number;
 
-  const calculator = (a:number, b: number, op: Operation) => {
-    if(op==='multiply'){
-      return a * b;
-    } else if(op==='add') {
-      return a + b;
-    } else if(op==="division"){
-      return a / b;
-    } 
-   
+  const calculator = (a: number, b: number, op : Operation) : Result => {
+    switch(op) {
+      case 'multiply':
+        return a * b;
+      case 'divide':
+        if( b === 0) throw new Error('Can\'t divide by 0!');
+        return a / b;
+      case 'add':
+        return a + b;
+      default:
+        throw new Error('Operation is not multiply, add or divide!');
+    }
   }
-
-  calculator(2, 4, 'null')
+  
+  try {
+    console.log(calculator(1, 5 , 'divide'))
+  } catch (e) {
+    console.log('Something went wrong, error message: ', e.message);
+  }
+  
