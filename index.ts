290c960627b21ac0b58eb65d258aa660c6a46f04
incredible92 <sulymanabdullahi@gmail.com>
2021-05-17 09:37:10 +0100

    update: code refactor

--- index.ts
+++ index.ts
@@ -6,7 +6,12 @@ app.get('/ping', (_req, res) => {
   res.send('pong');
 });
 
-
+app.post('/calculate', (req, res) => {
+    const { value1, value2, op } = req.body
+  
+    const result = calculator(value1, value2, op);
+    res.send(result);
+  });
 const PORT = 3003;
 
 app.listen(PORT, () => {
